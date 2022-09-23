1ad936d17821569237f4e6e18cad156b3ba48997
RomanPPP <st062267@student.spbu.ru>
2022-09-23 14:43:34 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const m3 = require('./src/m3')
 const m4 = require('./src/m4')
 const {Node , TRS}= require('./src/node')
 const vector = require('./src/vector')
+
+module.exports = {
+    AABB, m3, m4, Node, TRS, vector
+}
