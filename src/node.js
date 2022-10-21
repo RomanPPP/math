48966afd44a15c1d7979ef5a58d99f0e31e372d1
RomanPPP <st062267@student.spbu.ru>
2022-10-21 18:14:05 +0300

    update

--- src/node.js
+++ src/node.js
@@ -1,74 +1,82 @@
+const m4 = require("./m4");
+class TRS {
+  constructor(translation, rotation, scale) {
+    this.translation = translation;
+    this.rotation = rotation;
+    this.scale = scale;
+  }
+  getMatrix(m) {
+    let dst = m || m4.identity();
+    var t = this.translation;
+    var r = this.rotation;
+    var s = this.scale;
+    const sin = Math.sin(r[3] / 2);
+    const cos = Math.cos(r[3] / 2);
+    dst = m4.translate(dst, t[0], t[1], t[2]);
 
-const m4 = require('./m4')
-class TRS{
-    constructor(translation, rotation, scale){
-        this.translation = translation
-        this.rotation = rotation
-        this.scale = scale
-    }
-    getMatrix(m) {
+    dst = m4.multiply(dst, m4.fromQuaternion(r));
 
-        let dst = m || m4.identity();
-        var t = this.translation;
-        var r = this.rotation;
-        var s = this.scale;
-        const sin = Math.sin(r[3] / 2)
-        const cos = Math.cos(r[3] / 2)
-        dst = m4.translate(dst,t[0], t[1], t[2]);
+    dst = m4.scale(dst, s[0], s[1], s[2]);
+    return dst;
+  }
+  getRMatrix() {
+    let dst = m4.identity();
+    var r = this.rotation;
+    dst = m4.xRotate(dst, r[0]);
+    dst = m4.yRotate(dst, r[1]);
+    dst = m4.zRotate(dst, r[2]);
+    return dst;
+  }
+  getTRmatrix() {
+    const t = this.translation;
+    const r = this.rotation;
 
-        dst = m4.multiply(dst, m4.fromQuaternion(r))
-        
-        dst = m4.scale(dst, s[0], s[1], s[2]);
-        return dst;
-      };
-    getRMatrix(){
-        let dst = m4.identity();
-        var r = this.rotation;
-        dst = m4.xRotate(dst, r[0]);
-        dst = m4.yRotate(dst, r[1]);
-        dst = m4.zRotate(dst, r[2]);
-        return dst
-    }
+    let m = m4.translation(dst, t[0], t[1], t[2]);
+
+    m = m4.xRotate(m, r[0]);
+    m = m4.yRotate(m, r[1]);
+    m = m4.zRotate(m, r[2]);
+    return m
+  }
 }
-class Node{
-    constructor( name, trs = new TRS()){
-        this.worldMatrix = m4.identity()
-        this.parent = null
-        this.children = []
-        this.trs = trs
-        this.name = name
-        this.parts = []
+class Node {
+  constructor(name, trs = new TRS()) {
+    this.worldMatrix = m4.identity();
+    this.parent = null;
+    this.children = [];
+    this.trs = trs;
+    this.name = name;
+    this.parts = [];
+  }
+  setParent(parent) {
+    if (this.parent) {
+      const ndx = this.parent.children.indexOf(this);
+      if (ndx >= 0) {
+        this.parent.children.splice(ndx, 1);
+      }
     }
-    setParent(parent){
-        if (this.parent) {
-            const ndx = this.parent.children.indexOf(this);
-            if (ndx >= 0) {
-              this.parent.children.splice(ndx, 1);
-            }
-          }
-          if (parent) {
-            parent.children.push(this);
-          }
-          this.parent = parent;
+    if (parent) {
+      parent.children.push(this);
     }
-    updateWorldMatrix(parentWorldMatrix){
-        let matrix = this.trs.getMatrix()
-        if (parentWorldMatrix) {
-          matrix = m4.multiply(parentWorldMatrix, matrix)
-        }
-        this.worldMatrix = matrix
-        this.children.forEach((child) => {
-          child.updateWorldMatrix(matrix);
-        })
+    this.parent = parent;
+  }
+  updateWorldMatrix(parentWorldMatrix) {
+    let matrix = this.trs.getMatrix();
+    if (parentWorldMatrix) {
+      matrix = m4.multiply(parentWorldMatrix, matrix);
     }
-    updatePartsList(){
-        const iter = (node,arr) => {
-            arr.push(node)
-            node.children.forEach(child => iter(child,arr))
-        }
-        iter(this, this.parts)
-    }
+    this.worldMatrix = matrix;
+    this.children.forEach((child) => {
+      child.updateWorldMatrix(matrix);
+    });
+  }
+  updatePartsList() {
+    const iter = (node, arr) => {
+      arr.push(node);
+      node.children.forEach((child) => iter(child, arr));
+    };
+    iter(this, this.parts);
+  }
 }
 
-
-module.exports = {Node, TRS}
+module.exports = { Node, TRS };
